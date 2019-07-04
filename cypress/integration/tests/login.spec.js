6ce4a90ffddd0a2fd1bdf2a6bbe934243e7fb14b
Wilder Gonzales <wilder.gonzales@gmail.com>
2019-07-04 11:26:38 +0200

    read credentials from config file

--- cypress/integration/tests/login.spec.js
+++ cypress/integration/tests/login.spec.js
@@ -1,10 +1,12 @@
+const user = require('../../config/config.json').credentials;
+
 describe('gmail sign in', function() {
 
   it('should login successfully ', function() {
 
     cy.visit('https://mail.google.com/mail');
 
-    cy.get('#identifierId').type('test');
+    cy.get('#identifierId').type(user.email);
     cy.get('.RveJvd').click();
 
     cy.wait(1000);
